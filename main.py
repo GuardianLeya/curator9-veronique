c81ac658ddea55b88b3a703aaedc2064540af8a2
GuardianLeya <74779670+GuardianLeya@users.noreply.github.com>
2023-11-29 04:29:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ bot = telebot.TeleBot('6810377813:AAHBdWlT05IaWtj3vWd6WNiUKOfIMUDe-W0')
 def main(message):
     bot.send_message(message.chat.id, 'Привет:) Напиши /morning , если ты только что проснулся; напиши /night , если ты ложишься спать; напиши /niceday , и у тебя будет хороший день)', parse_mode='Markdown')
 
-@bot.message_handler(commands=['/morning'])
+@bot.message_handler(commands=['morning'])
 def main(message):
     bot.send_message(message.chat.id, 'Доброе утро:)', parse_mode='Markdown')
     
-@bot.message_handler(commands=['/night'])
+@bot.message_handler(commands=['night'])
 def main(message):
     bot.send_message(message.chat.id, 'Спокойной ночи:)', parse_mode='Markdown')
     
-@bot.message_handler(commands=['/niceday'])
+@bot.message_handler(commands=['niceday'])
 def main(message):
     bot.send_message(message.chat.id, '[Желаю хорошего дня:)](https://pozdravok.com/pozdravleniya/lyubov/dobroe-utro/khoroshego-dnya/proza.htm)', parse_mode='Markdown')
     
